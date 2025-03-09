84f9fe6a55a581ef4a63a93fab438c4a87b9bc85
varunchimurkar <varunchimurkar96@gmail.com>
2025-03-09 08:34:08 +0530

    Challenge-2 Done

--- challenge-2/app.js
+++ challenge-2/app.js
@@ -1,3 +1,33 @@
 /**
  * Write your challenge solution here
  */
+
+const colorbutton = document.getElementById('color-buttons')
+const changecolor = document.getElementById('mainHeading')
+const red = document.getElementById('redButton')
+const green = document.getElementById('greenButton')
+const blue = document.getElementById('blueButton')
+const purple = document.getElementById('purpleButton')
+const reset = document.getElementById('resetButton')
+
+
+red.addEventListener('click', () => {
+    changecolor.style.color = 'red'
+})
+
+green.addEventListener('click', () => {
+    changecolor.style.color = 'green'
+})
+
+blue.addEventListener('click', () => {
+    changecolor.style.color = 'blue'
+})
+
+purple.addEventListener('click', () => {
+    changecolor.style.color = 'purple'
+})
+
+reset.addEventListener('click', () => {
+    changecolor.style.color = ''
+})
+
