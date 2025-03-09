07146084bc9a1af923bf8c6425dd91594968542e
varunchimurkar <varunchimurkar96@gmail.com>
2025-03-09 16:44:46 +0530

    Challenge-3 Done

--- challenge-3/app.js
+++ challenge-3/app.js
@@ -1,3 +1,27 @@
 /**
  * Write your challenge solution here
  */
+const ndisplay = document.getElementById('nameDisplay')
+const jdisplay = document.getElementById('jobDisplay')
+const agedisplay = document.getElementById('ageDisplay')
+const biodisplay = document.getElementById('bioDisplay')
+
+
+
+nameInput.addEventListener('input', function () {
+    ndisplay.textContent = nameInput.value;
+})
+
+jobInput.addEventListener('input', function () {
+    jdisplay.textContent = jobInput.value;
+})
+
+ageInput.addEventListener('input', function () {
+    agedisplay.textContent = ageInput.value;
+})
+
+bioInput.addEventListener('input', function () {
+    biodisplay.textContent = bioInput.value;
+})
+
+
